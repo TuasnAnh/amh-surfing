c2a17f433fc0a0d4a2d0e76ab5c1e75e6f6f5f72
Tuasn Anh <tuananhblablo@gmail.com>
2020-05-21 15:28:34 +0700

    init

---
+++ precache-manifest.ce83fa3660432a96178a29508fd29ac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317e901f5c2cc1b277c6823d93b7953d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001095e05d426025350b",
+    "url": "/static/css/main.6055004f.chunk.css"
+  },
+  {
+    "revision": "9d46bb88239289d50a2d",
+    "url": "/static/js/2.3b2d5d87.chunk.js"
+  },
+  {
+    "revision": "d9b445fd12d8bcaca03d304ff543ec4e",
+    "url": "/static/js/2.3b2d5d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001095e05d426025350b",
+    "url": "/static/js/main.27e129e3.chunk.js"
+  },
+  {
+    "revision": "e0deff6561e7b5eccb55",
+    "url": "/static/js/runtime-main.0cf56d1a.js"
+  }
+]);
